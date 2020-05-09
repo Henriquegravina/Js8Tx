eddfe36367732b485f83ad67bbd08a1c10434abe
Henrique Gravina <henrique@gravina.com.br>
2020-05-09 19:27:49 -0300

    New version
    * Add arduino timing for TX
    * Send all codes at once to Arduino
    * Read from JS8call and send QRG, Offset and Speed mode to Arduino

--- Js8Tx.py
+++ Js8Tx.py
@@ -2,8 +2,10 @@ import socket
 import json
 import time
 import serial
+import time, threading
 
-ser = serial.Serial('/dev/ttyUSB2', 19200)
+    
+ser = serial.Serial('/dev/ttyUSB1', 19200)
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 2242
@@ -12,16 +14,40 @@ sock = socket.socket(socket.AF_INET,
                      socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
+
+
 while True:
     data, addr = sock.recvfrom(1024) 
     message = json.loads(data)
+    
+    print message
     if(message['type'] == 'TX.FRAME'):
         print(message['params']['TONES'])
         symbols = message['params']['TONES']
+        #ser.write("SYMBOLS 811111111112222222222333333333344444444445555555555666666666677777777771111111119\r".encode())
+        ser.write("SYMBOLS ")
         ser.write("8")
         for symbol in symbols:
-            time.sleep(12.64 / 79) # 12.64 = Normal Mode Speed 25.28 = Slow
             print symbol
             ser.write(str(symbol))
-        ser.write("9")
+        ser.write("9\r".encode())
+    
+    if(message['type'] == 'PING'): # PING MESSAGE 
+        print("Ping received")
+        # When we receive a ping from JS8Call we can ask for this:
+        sock.sendto(json.dumps({'type': 'RIG.GET_FREQ'}), addr)    #QRG and Offset
+        sock.sendto(json.dumps({'type': 'MODE.GET_SPEED'}), addr)  #Offset
+        
+    if(message['type'] == 'RIG.FREQ'): # RIG.FREQ MESSAGE
+        print("QRG:" + str(message['params']['DIAL']))
+        ser.write("QRG " + str(message['params']['DIAL']) + "\r".encode() ) #Send QRG to Arduino
 
+        print("OFFSET: " + str(message['params']['OFFSET']))
+        ser.write("OFFSET " + str(message['params']['OFFSET']) + "\r".encode() ) # Send offset to Arduino
+
+    if(message['type'] == 'MODE.SPEED'): # MODE.SPEED message
+        #SPEED NORMAL = 0, SLOW = 4, FAST = 1, TURBO = 2
+        print("SPEED: " + str(message['params']['SPEED']))
+        ser.write("SPEED " + str(message['params']['SPEED']) + "\r".encode() )
+    
+        
--- tx_js8.ino
+++ tx_js8.ino
@@ -1,48 +1,131 @@
 #include <AD9850SPI.h>
 #include <SPI.h>
 
+// AD9850 config
 const int W_CLK_PIN = 13;
 const int FQ_UD_PIN = 8;
 const int RESET_PIN = 9;
-
-double freq = 3578000;
-//double freq = 7044000;
-//double freq = 28078000;
-double offset = 1000;
 double trimFreq = 125000000;
-double fout;
-int rxbyte;
-
 int phase = 0;
 
-void setup(){
-  Serial.begin(19200);
+//FS8 config
+double qrg = 7044000;
+double offset = 1000;
+double fout;
+int fs8_speed = 0;
+double tx_time = 12.64; // Normal
+double symbol_size = 6.25; // Normal
+double qrg_correction = 188;
+
+
+//Terminal Serial
+String command;
+char symbols[81];
+int symbol;
+int i;
+
+// Hardware setup
+int PTT_pin = 3;
+
+
+void setup() {
+
+  Serial.begin(19200); // Set serial speed
+  Serial.println("Start");
+  Serial.println(sizeof(symbols));
+  
+  pinMode(PTT_pin,OUTPUT);
+  digitalWrite(PTT_pin,LOW); // Set PTT pin to LOW
+
+  // Config AD9850
   DDS.begin(W_CLK_PIN, FQ_UD_PIN, RESET_PIN);
   DDS.calibrate(trimFreq);
-  DDS.setfreq(freq, phase);
+  DDS.setfreq(qrg, phase);
   DDS.down();
+
 }
 
-//  DDS.up();
-//  DDS.setfreq(freq, phase);
-  
-void loop(){
 
-if(Serial.available() > 0){
-   // Bytes do JS8 0..7, entoa uso 8 para ligar o DDS e PTT e 9 Para desligar
-   rxbyte = Serial.read() - 48;
-   if(rxbyte == 8){
-    DDS.up();
-   }else if(rxbyte ==9){
-    DDS.down();
-   }else {
-      fout = freq + offset + (6.25 * rxbyte);
-      DDS.setfreq(fout, phase);
-      Serial.print("freq: ");
-      Serial.println(fout);
-   }
+
+void txSymbols(){
+  
+  for(i = 0; i <= (sizeof(symbols)-1); i++){
+    symbol = symbols[i];
+    switch (symbols[i]) {
+      case 8:
+        Serial.print("TXON: ");
+        digitalWrite(3,HIGH);
+        DDS.up();
+        break;
+  
+      case 9:
+        Serial.println(" :TXOFF"); 
+        DDS.down();
+        digitalWrite(3,LOW);
+        break;
+  
+      default:
+        Serial.print(symbol);
+        fout = (qrg - qrg_correction ) + offset + (symbol_size * symbol);
+        DDS.setfreq(fout, phase);
+        Serial.print("freq: ");
+        Serial.println(fout);
+        delay( (tx_time)/79 * 1000 );
+    }
   }
 }
 
-  
-  
+void parseCommand(String com)
+{
+     String part1;
+     String part2;
+
+     part1 = com.substring(0, com.indexOf(" "));
+     part2 = com.substring(com.indexOf(" ")+1);
+
+     // symbols 811111111112222222222333333333344444444445555555555666666666677777777771111111119
+     if(part1.equalsIgnoreCase(F("SYMBOLS"))){      
+      Serial.print(F("Symbols: "));
+      for(i = 0; i <= (sizeof(symbols)-1); i++){
+        symbols[i] = part2[i] - 48;
+      }
+      txSymbols();
+     
+            
+      // SSID Setup 
+     }else if(part1.equalsIgnoreCase(F("QRG"))){
+      qrg = part2.toDouble();
+      Serial.print(F("QRG: "));
+      Serial.println(qrg);
+      
+     }
+     else if(part1.equalsIgnoreCase(F("OFFSET"))){
+      offset = part2.toDouble();
+      Serial.print(F("Offset: "));
+      Serial.println(offset);
+      
+     }else if(part1.equalsIgnoreCase(F("SPEED"))){
+      fs8_speed = part2.toInt();
+      Serial.print(F("Speed: "));
+      Serial.println(offset);
+      if(fs8_speed == 4) { tx_time = 25.28; symbol_size = 3.125; } // Slow Mode Speed
+      else if(fs8_speed == 0) { tx_time = 12.64; symbol_size = 6.25; } //Normal Mode Speed
+      else if(fs8_speed == 1) { tx_time = 7.9; symbol_size = 10; } // Fast Mode Speed
+      else if(fs8_speed == 2) { tx_time = 3.95; symbol_size = 20; } // Turbo Mode Speed
+      
+     }
+     
+}
+
+void loop() {     
+  if(Serial.available() > 0){
+    char c = Serial.read();
+    if( c == 13 ){
+      parseCommand(command);
+      command = "";
+    }else if( c == 10) { /*Do nothing*/  // To use Line feed
+    }else { command += c; 
+    }
+  }    
+}
+      
